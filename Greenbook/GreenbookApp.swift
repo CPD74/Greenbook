8e8555df3c9825fee199f60319fd8e6b7d437b4a
CPD74 <charliepdaniel@gmail.com>
2025-03-24 17:31:05 -0400

    Initial Commit

---
+++ Greenbook/GreenbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenbookApp.swift
+//  Greenbook
+//
+//  Created by Charlie Daniel on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct GreenbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GreenbookTests/GreenbookTests.swift
@@ -0,0 +1,17 @@
+//
+//  GreenbookTests.swift
+//  GreenbookTests
+//
+//  Created by Charlie Daniel on 3/24/25.
+//
+
+import Testing
+@testable import Greenbook
+
+struct GreenbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
