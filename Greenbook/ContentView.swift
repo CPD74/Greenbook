09b78b74fe36d60ba9394c32e7abe7883567ed78
CPD74 <charliepdaniel@gmail.com>
2025-03-25 09:46:16 -0400

    Connected Firebase with Greenbook App

--- Greenbook/ContentView.swift
+++ Greenbook/ContentView.swift
@@ -13,9 +13,10 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Greenbook test")
+            Text("Greenbook")
         }
         .padding()
+        
     }
 }
 
